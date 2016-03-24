f922a4553967360887c5e7a3220b6626a402b79d
Hiroki Kumamoto <kumabook@live.jp>
2016-03-24 23:52:21 +0900

    uenc and udec handle stdin

--- src/udec.rs
+++ src/udec.rs
@@ -2,12 +2,21 @@ extern crate url;
 
 use url::percent_encoding::{lossy_utf8_percent_decode};
 use std::env;
+use std::io::{self, Read};
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() < 2 {
+        let mut buffer = String::new();
+        match io::stdin().read_to_string(&mut buffer) {
+            Ok(_)  => println!("{}", decode(&buffer)),
+            Err(_) => println!(""),
+        }
         return;
     }
-    let decoded = lossy_utf8_percent_decode(args[1].as_bytes());
-    println!("{}", decoded);
+    println!("{}", decode(&args[1]));
+}
+
+fn decode(str: &String) -> String {
+    return lossy_utf8_percent_decode(str.as_bytes());
 }
--- src/uenc.rs
+++ src/uenc.rs
@@ -2,13 +2,22 @@ extern crate url;
 
 use url::percent_encoding::{percent_encode, FORM_URLENCODED_ENCODE_SET};
 use std::env;
+use std::io::{self, Read};
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() < 2 {
+        let mut buffer = String::new();
+        match io::stdin().read_to_string(&mut buffer) {
+            Ok(_)  => println!("{}", encode(&buffer)),
+            Err(_) => println!(""),
+        }
         return;
     }
-    let encoded = percent_encode(args[1].as_bytes(),
-                                 FORM_URLENCODED_ENCODE_SET);
-    println!("{}", encoded);
+    println!("{}", encode(&args[1]));
+}
+
+fn encode(str: &String) -> String {
+    return percent_encode(str.as_bytes(),
+                          FORM_URLENCODED_ENCODE_SET);
 }
